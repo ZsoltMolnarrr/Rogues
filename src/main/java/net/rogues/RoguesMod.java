6d979cc9c714266c895ca0d4dadcc7344bdd1786
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2024-04-26 16:33:17 +0200

    Add project base

---
+++ src/main/java/net/rogues/RoguesMod.java
@@ -0,0 +1,10 @@
+package net.rogues;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RoguesMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/rogues/client/RoguesClient.java
@@ -0,0 +1,10 @@
+package net.rogues.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoguesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
