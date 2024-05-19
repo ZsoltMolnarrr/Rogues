e5545f18e18bd96dde82e0fd485600d5b82e16d0
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2024-05-20 01:43:45 +0200

    Add structures

--- src/main/java/net/rogues/config/Default.java
+++ src/main/java/net/rogues/config/Default.java
@@ -18,21 +18,21 @@ public class Default {
         var smallWeight = 25;
         var limit = 1;
         villages.entries = new ArrayList<>(List.of(
-//                new StructurePoolConfig.Entry("minecraft:village/desert/houses", new ArrayList<>(Arrays.asList(
-//                        new StructurePoolConfig.Entry.Structure("rogues:village/desert/barracks", smallWeight, limit))
-//                )),
-//                new StructurePoolConfig.Entry("minecraft:village/savanna/houses", new ArrayList<>(Arrays.asList(
-//                        new StructurePoolConfig.Entry.Structure("rogues:village/savanna/barracks", smallWeight, limit))
-//                )),
+                new StructurePoolConfig.Entry("minecraft:village/desert/houses", new ArrayList<>(Arrays.asList(
+                        new StructurePoolConfig.Entry.Structure("rogues:village/desert/barracks", smallWeight, limit))
+                )),
+                new StructurePoolConfig.Entry("minecraft:village/savanna/houses", new ArrayList<>(Arrays.asList(
+                        new StructurePoolConfig.Entry.Structure("rogues:village/savanna/barracks", smallWeight, limit))
+                )),
                 new StructurePoolConfig.Entry("minecraft:village/plains/houses", new ArrayList<>(Arrays.asList(
                         new StructurePoolConfig.Entry.Structure("rogues:village/plains/barracks", smallWeight, limit))
                 )),
                 new StructurePoolConfig.Entry("minecraft:village/taiga/houses", new ArrayList<>(Arrays.asList(
                         new StructurePoolConfig.Entry.Structure("rogues:village/taiga/barracks", smallWeight, limit))
+                )),
+                new StructurePoolConfig.Entry("minecraft:village/snowy/houses", new ArrayList<>(Arrays.asList(
+                        new StructurePoolConfig.Entry.Structure("rogues:village/snowy/barracks", smallWeight, limit))
                 ))
-//                new StructurePoolConfig.Entry("minecraft:village/snowy/houses", new ArrayList<>(Arrays.asList(
-//                        new StructurePoolConfig.Entry.Structure("rogues:village/snowy/barracks", smallWeight, limit))
-//                ))
         ));
 
     }
