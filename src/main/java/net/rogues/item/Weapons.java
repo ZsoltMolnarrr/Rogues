f27345ec0a2d5da8a2a7f71a5403674a00c237f5
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2024-06-02 23:45:20 +0200

    Balance 1H weapons

--- src/main/java/net/rogues/item/Weapons.java
+++ src/main/java/net/rogues/item/Weapons.java
@@ -60,38 +60,38 @@ public class Weapons {
     private static Weapon.Entry dagger(String requiredMod, String name, Weapon.CustomMaterial material, float damage) {
         var settings = new Item.Settings();
         var item = new MeleeWeaponItem(material, settings);
-        return entry(requiredMod, name, material, item, new ItemConfig.Weapon(damage, -2.8F));
+        return entry(requiredMod, name, material, item, new ItemConfig.Weapon(damage, -1.6F));
     }
 
     public static final Weapon.Entry flint_dagger = dagger("flint_dagger",
-            Weapon.CustomMaterial.matching(ToolMaterials.IRON, () -> Ingredient.ofItems(Items.FLINT)), 6F);
+            Weapon.CustomMaterial.matching(ToolMaterials.IRON, () -> Ingredient.ofItems(Items.FLINT)), 2.6F);
     public static final Weapon.Entry iron_dagger = dagger("iron_dagger",
-            Weapon.CustomMaterial.matching(ToolMaterials.IRON, () -> Ingredient.ofItems(Items.IRON_INGOT)), 6F);
+            Weapon.CustomMaterial.matching(ToolMaterials.IRON, () -> Ingredient.ofItems(Items.IRON_INGOT)), 3.3F);
     public static final Weapon.Entry golden_dagger = dagger("golden_dagger",
-            Weapon.CustomMaterial.matching(ToolMaterials.GOLD, () -> Ingredient.ofItems(Items.GOLD_INGOT)), 5F);
+            Weapon.CustomMaterial.matching(ToolMaterials.GOLD, () -> Ingredient.ofItems(Items.GOLD_INGOT)), 1.8F);
     public static final Weapon.Entry diamond_dagger = dagger("diamond_dagger",
-            Weapon.CustomMaterial.matching(ToolMaterials.DIAMOND, () -> Ingredient.ofItems(Items.DIAMOND)), 8F);
+            Weapon.CustomMaterial.matching(ToolMaterials.DIAMOND, () -> Ingredient.ofItems(Items.DIAMOND)), 4F);
     public static final Weapon.Entry netherite_dagger = dagger("netherite_dagger",
-            Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, () -> Ingredient.ofItems(Items.NETHERITE_INGOT)), 9F);
+            Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, () -> Ingredient.ofItems(Items.NETHERITE_INGOT)), 4.7F);
 
     private static Weapon.Entry sickle(String name, Weapon.CustomMaterial material, float damage) {
-        return dagger(null, name, material, damage);
+        return sickle(null, name, material, damage);
     }
 
     private static Weapon.Entry sickle(String requiredMod, String name, Weapon.CustomMaterial material, float damage) {
         var settings = new Item.Settings();
         var item = new MeleeWeaponItem(material, settings);
-        return entry(requiredMod, name, material, item, new ItemConfig.Weapon(damage, -2.8F));
+        return entry(requiredMod, name, material, item, new ItemConfig.Weapon(damage, -2F));
     }
 
     public static final Weapon.Entry iron_sickle = sickle("iron_sickle",
-            Weapon.CustomMaterial.matching(ToolMaterials.IRON, () -> Ingredient.ofItems(Items.IRON_INGOT)), 6F);
+            Weapon.CustomMaterial.matching(ToolMaterials.IRON, () -> Ingredient.ofItems(Items.IRON_INGOT)), 4.1F);
     public static final Weapon.Entry golden_sickle = sickle("golden_sickle",
-            Weapon.CustomMaterial.matching(ToolMaterials.GOLD, () -> Ingredient.ofItems(Items.GOLD_INGOT)), 5F);
+            Weapon.CustomMaterial.matching(ToolMaterials.GOLD, () -> Ingredient.ofItems(Items.GOLD_INGOT)), 2.4F);
     public static final Weapon.Entry diamond_sickle = sickle("diamond_sickle",
-            Weapon.CustomMaterial.matching(ToolMaterials.DIAMOND, () -> Ingredient.ofItems(Items.DIAMOND)), 8F);
+            Weapon.CustomMaterial.matching(ToolMaterials.DIAMOND, () -> Ingredient.ofItems(Items.DIAMOND)), 5F);
     public static final Weapon.Entry netherite_sickle = sickle("netherite_sickle",
-            Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, () -> Ingredient.ofItems(Items.NETHERITE_INGOT)), 9F);
+            Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, () -> Ingredient.ofItems(Items.NETHERITE_INGOT)), 5.9F);
 
     // MARK: Double Axe
 
@@ -144,16 +144,16 @@ public class Weapons {
     public static void register(Map<String, ItemConfig.Weapon> configs) {
         if (RoguesMod.tweaksConfig.value.ignore_items_required_mods || FabricLoader.getInstance().isModLoaded(BETTER_END)) {
             var repair = ingredient("betterend:aeternium_ingot", FabricLoader.getInstance().isModLoaded(BETTER_END), Items.NETHERITE_INGOT);
-            dagger("aeternium_dagger", Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, repair), 9F);
-            sickle("aeternium_sickle", Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, repair), 9F);
+            dagger("aeternium_dagger", Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, repair), 5.5F);
+            sickle("aeternium_sickle", Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, repair), 6.8F);
             axe("aeternium_double_axe", Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, repair), 13F);
             glaive("aeternium_glaive", Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, repair), 11F);
         }
 
         if (RoguesMod.tweaksConfig.value.ignore_items_required_mods || FabricLoader.getInstance().isModLoaded(BETTER_NETHER)) {
             var repair = ingredient("betternether:nether_ruby", FabricLoader.getInstance().isModLoaded(BETTER_END), Items.NETHERITE_INGOT);
-            dagger("ruby_dagger", Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, repair), 9F);
-            sickle("ruby_sickle", Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, repair), 9F);
+            dagger("ruby_dagger", Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, repair), 5.5F);
+            sickle("ruby_sickle", Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, repair), 6.8F);
             axe("ruby_double_axe", Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, repair), 13F);
             glaive("ruby_glaive", Weapon.CustomMaterial.matching(ToolMaterials.NETHERITE, repair), 11F);
         }
