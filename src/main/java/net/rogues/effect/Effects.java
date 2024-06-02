7d1c748fa73606f8e43c615679ab59c35955cea7
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2024-06-02 12:43:57 +0200

    Add vanish

--- src/main/java/net/rogues/effect/Effects.java
+++ src/main/java/net/rogues/effect/Effects.java
@@ -24,6 +24,11 @@ public class Effects {
                     -1F,
                     EntityAttributeModifier.Operation.MULTIPLY_BASE);
     public static int sliceAndDiceMaxStacks = 10;
+    public static final StatusEffect STEALTH = new CustomStatusEffect(StatusEffectCategory.BENEFICIAL, 0xAAAAAA)
+            .addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED,
+                    "112f3133-8a44-11ed-a1eb-0242ac120003",
+                    -0.5F,
+                    EntityAttributeModifier.Operation.MULTIPLY_BASE);
 
     public static void register() {
         Synchronized.configure(SLICE_AND_DICE, true);
@@ -33,5 +38,6 @@ public class Effects {
         int rawId = 753;
         Registry.register(Registries.STATUS_EFFECT, rawId++, new Identifier(RoguesMod.NAMESPACE, "slice_and_dice").toString(), SLICE_AND_DICE);
         Registry.register(Registries.STATUS_EFFECT, rawId++, new Identifier(RoguesMod.NAMESPACE, "shock").toString(), SHOCK);
+        Registry.register(Registries.STATUS_EFFECT, rawId++, new Identifier(RoguesMod.NAMESPACE, "stealth").toString(), STEALTH);
     }
 }
