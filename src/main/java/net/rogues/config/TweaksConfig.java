e6cc695d5a8a111298c927c71b897442d8de680e
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2024-06-18 21:35:35 +0200

    Rebalance vanilla damage enchantments

--- src/main/java/net/rogues/config/TweaksConfig.java
+++ src/main/java/net/rogues/config/TweaksConfig.java
@@ -11,4 +11,10 @@ public class TweaksConfig { public TweaksConfig() {}
     public double shout_damage_multiplier = -0.3;
     public double charge_speed_multiplier = 0.5;
     public double charge_knockback_resistance_bonus = 0.5F;
+    public boolean enable_rebalance_enchantment_sharpness = true;
+    public float enchantment_sharpness_multiplier_per_level = 0.08F;
+    public boolean enable_rebalance_enchantment_smite = true;
+    public float enchantment_smite_multiplier_per_level = 0.12F;
+    public boolean enable_rebalance_enchantment_arthropods = true;
+    public float enchantment_arthropods_multiplier_per_level = 0.12F;
 }
